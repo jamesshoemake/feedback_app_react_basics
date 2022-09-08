e1e45878e2955b65df5d08abd1bd4f0981ea0ac0
James Shoemake <james.shoemake1@gmail.com>
2022-09-08 11:05:33 -0500

    Added Ability to see feedback items, ability to add and remove feedback, shared components Button and Card.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { v4 as uuidv4 } from 'uuid'
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+
 const App = () => {
-  return <div>App</div>
+  const [feedback, setFeedback] = useState(FeedbackData)
+
+  const deleteFeedback = (id) => {
+    if (window.confirm(`delete ${id}?`)) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+
+const FeedbackStats = ({ feedback }) => {
+  //  calc rating average
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating
+    }, 0) / feedback.length
+
+  //  remove trailing 0s
+  average = average.toFixed(1).replace(/[.,]0$/, '')
+
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating {isNaN(average) ? '0' : average}</h4>
+    </div>
+  )
+}
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
+  )
+}
+
+export default FeedbackStats
